a18020b0524f677572c10b7434d1288a14df488f
Lingeshwaran2112 <85979196+Lingeshwaran2112@users.noreply.github.com>
2021-12-11 16:50:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+let timer = document.getElementById('countDown');
+let begin = 15;
+
+setTimeout(() => {
+    timer.innerHTML = begin;
+    begin--;
+    setTimeout(() => {
+        timer.innerHTML = begin;
+        begin--;
+        setTimeout(() => {
+            timer.innerHTML = begin;
+            begin--;
+            setTimeout(() => {
+                timer.innerHTML = begin;
+                begin--;
+                setTimeout(() => {
+                    timer.innerHTML = begin;
+                    begin--;
+                    setTimeout(() => {
+                        timer.innerHTML = begin;
+                        begin--;
+                        setTimeout(() => {
+                            timer.innerHTML = begin;
+                            begin--;
+                            setTimeout(() => {
+                                timer.innerHTML = begin;
+                                begin--;
+                                setTimeout(() => {
+                                    timer.innerHTML = begin;
+                                    begin--;
+                                    setTimeout(() => {
+                                        timer.innerHTML = begin;
+                                        begin--;
+                                        setTimeout(() => {
+                                            timer.innerHTML = begin;
+                                            begin--;
+                                            setTimeout(() => {
+                                                timer.innerHTML = begin;
+                                                begin--;
+                                                setTimeout(() => {
+                                                    timer.innerHTML = begin;
+                                                    begin--;
+                                                    setTimeout(() => {
+                                                        timer.innerHTML = begin;
+                                                        begin--;
+                                                        setTimeout(() => {
+                                                            timer.innerHTML = begin;
+                                                            begin--;
+                                                            setTimeout(() => {
+                                                                timer.innerHTML = 'Happy New Year';
+                                                                timer.classList.add('text-danger')
+                                         
+                                                            }, 1000)
+                                                        }, 1000)
+                                                    }, 1000)
+                                                }, 1000)
+                                            }, 1000)
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000) 
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
